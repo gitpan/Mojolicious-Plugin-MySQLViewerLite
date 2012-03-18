165e625f90ce175aea99d9ffc7d01579f1a95b5c
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-03-18 09:15:36 +0000

    Import of KIMOTO/Mojolicious-Plugin-MySQLViewerLite-0.09 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-MySQLViewerLite
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         KIMOTO/Mojolicious-Plugin-MySQLViewerLite-0.09.tar.gz
    gitpan-cpan-author:       KIMOTO
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/MySQLViewerLite.pm
+++ lib/Mojolicious/Plugin/MySQLViewerLite.pm
@@ -5,7 +5,7 @@ use File::Basename 'dirname';
 use Cwd 'abs_path';
 use Mojolicious::Plugin::MySQLViewerLite::Command;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 has command => sub {
   my $self = shift;
